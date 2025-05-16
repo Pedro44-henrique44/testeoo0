3da89e1510bbff1861e0bcde4a42f522d7800684
Pedro Henrique <pedruhenrique56@gmail.com>
2025-05-16 17:47:28 -0300

    pesquisa de satisfação, atualização

--- xandji.py
+++ xandji.py
@@ -1,2 +1,6 @@
 import requests
 print("Meu programa é relevante para a sociedade moderna!")
+
+nota = input("de uma escola de 0 à 100, como você avalia a relevância desse projeto?")
+
+print(f"esse projeto tem uma relevância de {nota}")
